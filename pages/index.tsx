71a4142dbb7d2ac3738b97db3cf70cf7ab2d0c8a
Yan Ceican <baka_@bk.ru>
2021-08-18 14:11:50 +0300

    Добавление компнента Р

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,9 @@ export default function Home(): JSX.Element {
       <HTag tag='h1'>Заголовок первого уровня</HTag>
       <Button apperanse='primary' onClick={handleClick} arrow='down'>primary button</Button>
       <Button apperanse='ghost' arrow='right'>ghost button</Button>
-      <P size='s'>test test test test test test </P>
+      <P size='s'>Маленький Маленький Маленький Маленький Маленький</P>
+      <P>Средний Средний Средний Средний Средний Средний Средний</P>
+      <P size='l'>Большой Большой Большой Большой Большой Большой</P>
     </div>
   );
 }
